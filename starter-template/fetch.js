e7dcf95d4c457eb6ed630f31813ff50cd84b1a15
EliezerDev <ardhiputrapradana21@gmail.com>
2021-05-21 07:46:44 +0700

    add code starter template

---
+++ starter-template/fetch.js
@@ -0,0 +1,8 @@
+const getBtn = document.getElementById("get-btn");
+const postBtn = document.getElementById("post-btn");
+
+const getData = () => {};
+const postData = () => {};
+
+getBtn.addEventListener("click", getData);
+postBtn.addEventListener("click", postData);
---
+++ starter-template/xhr.js
@@ -0,0 +1,9 @@
+const getBtn = document.getElementById("get-btn");
+const postBtn = document.getElementById("post-btn");
+
+const getData = () => {};
+const postData = () => {};
+
+console.log("okee");
+getBtn.addEventListener("click", getData);
+postBtn.addEventListener("click", postData);
